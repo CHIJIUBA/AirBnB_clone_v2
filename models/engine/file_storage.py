b96f7f8702235bc61daf0700baa2abe49e991a1a
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-10-11 12:29:01 +0100

    go

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,6 +22,17 @@ class FileStorage:
         Returns dictionary representation of all objects
         """
         return self.__objects
+    
+    # def all(self, cls=None):
+    #     """Returns a dictionary of models currently in storage"""
+    #     if cls is None:
+    #         return self.__objects
+    #     cls_name = cls.__name__
+    #     dct = {}
+    #     for key in self.__objects.keys():
+    #         if key.split('.')[0] == cls_name:
+    #             dct[key] = self.__objects[key]
+    #     return dct
 
     def new(self, object):
         """sets in __objects the object with the key
